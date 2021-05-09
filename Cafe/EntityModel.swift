11b60e49e066667bd5f16ff11fd6cf4cdc611bed
dev <dev@devs-iMac.local>
2021-05-08 21:16:40 -0700

    fixes

---
+++ Cafe/EntityModel.swift
@@ -0,0 +1,16 @@
+//
+//  MainHomeViewController.swift
+//  Cafe-Manager
+//
+//  Created by dev on 5/2/21.
+//  Copyright © 2021 dev. All rights reserved.
+//
+
+import Foundation
+
+struct User{
+    var userEmail: String
+    var userPassword: String
+    var userPhone: String
+    
+}
--- Cafe/RegisterController.swift
+++ Cafe/RegisterController.swift
@@ -7,36 +7,56 @@
 //
 
 import UIKit
+import FirebaseDatabase
 import Firebase
 
-class RegisterController: UIViewController {
 
+class RegisterController: UIViewController {
+    var ref = Database.database().reference()
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var phone: UITextField!
     @IBOutlet weak var password: UITextField!
     
     @IBAction func BtnRegister(_ sender: UIButton) {
-        if let email = email.text, let password = password.text {
-            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-                if let e = error {
-                    print(e)
-                    let alert = UIAlertController(title: "Error", message: e.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+        if email.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || password.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || phone.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""
+                {
+                    let alert = UIAlertController(title: "Error", message: "All fields required", preferredStyle: UIAlertController.Style.alert)
                     alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
-                    self.present(alert, animated: true, completion: nil)
-                } else {
-                    let db = Firestore.firestore()
-                    db.collection("Userinfos").addDocument(data: ["phone" : self.phone.text ,"user_id" : authResult!.user.uid ,"user_email" : authResult!.user.email, "user_type" : "customer" ]) { (error) in
-                        if error != nil
-                        {
+                    self.present(alert, animated: true, completion: nil)}
+                else{
+                if let email = email.text, let password = password.text {
+                    Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                        if let e = error {
+                            print(e)
+                            let alert = UIAlertController(title: "Error", message: e.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+                            self.present(alert, animated: true, completion: nil)
+                        } else {
+                            let user = User(userEmail: email , userPassword: password , userPhone: self.phone.text ?? "")
+                            let userData = [
+                                "userEmail" : user.userEmail,
+                                "userPhone" : user.userPhone,
+                                "userPassword" : user.userPassword
+                            ]
                             
-                        } else{
+                            self.ref.child("users")
+                                .child(user.userEmail
+                                    .replacingOccurrences(of: "@", with: "_")
+                                    .replacingOccurrences(of: ".", with: "_")).setValue(userData) {
+                            (error, ref) in
+                            if let err = error {
+                                print(err.localizedDescription)
+                                let alert = UIAlertController(title: "Error", message: err.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+                                self.present(alert, animated: true, completion: nil)
+                            }
                             self.performSegue(withIdentifier: "RegisterToLocation", sender: self)
                         }
                     }
                 }
             }
+            }
         }
-    }
     
 
 }
